8505e8dac61ef2ac7a7015238ba4006fca5b34a8
janipang <pang.janista@gmail.com>
2023-07-16 23:13:25 +0700

    kritsada still survived

--- chap03/ex05_InTheWoods.py
+++ chap03/ex05_InTheWoods.py
@@ -18,8 +18,13 @@ class Stack():
 	
 	def peek(self):
 		if self.is_empty():
-			return None
+			return -1
 		return self.stack[-1]
+
+	def max(self):
+		if self.is_empty():
+			return -1
+		return max(self.stack)
 	
 	def __len__(self):
 		return len(self.stack)
@@ -27,16 +32,33 @@ class Stack():
 	def __str__(self):
 		return str(self.stack)
 
-command = list(map(str, input("Enter Input : ").split(',')))
-
 def count_tree(cmd):
-    back_tree = Stack()
-    mao_hed = Stack()
-    for ele in cmd:
-        mode = ele[0]
-        if mode == 'A':
-            pass
-        if mode == 'B':
-            pass
-        if mode == 'S':
-            pass
+	passed_tree = Stack()
+	mao_hed = Stack()
+	look_back = Stack()
+	for ele in cmd:
+		mode = ele[0]
+		if mode == 'A':
+			new_tree = int(ele[2:])
+			passed_tree.push(new_tree)
+		if mode == 'B':
+			count = 0
+			while not passed_tree.is_empty():
+				if passed_tree.peek() > look_back.max():
+					count += 1
+				look_back.push(passed_tree.pop())
+			while not look_back.is_empty():
+				passed_tree.push(look_back.pop())
+			print(count)
+		if mode == 'S':
+			while not passed_tree.is_empty():
+				if passed_tree.peek() % 2 == 0:
+					if passed_tree.peek() > 1:
+						mao_hed.push(passed_tree.pop() - 1)
+				else:
+					mao_hed.push(passed_tree.pop() + 2)
+			while not mao_hed.is_empty():
+				passed_tree.push(mao_hed.pop())
+    
+command = list(map(str, input("Enter Input : ").split(',')))
+count_tree(command)
